2ac9c94893f0320e0043df1f2a3f7e8e33333d40
nastyafureeva <AnaDFureeva@stud.kpfu.ru>
2023-12-12 21:50:36 +0300

    added onClick and initialize

--- Game/src/main/java/org/example/field/game/HelloController.java
+++ Game/src/main/java/org/example/field/game/HelloController.java
@@ -1,14 +1,67 @@
 package org.example.field.game;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private Pane man;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Pane base1;
+    @FXML
+    private Pane base2;
+    @FXML
+    private Pane base3;
+    @FXML
+    private Pane pole;
+    @FXML
+    private Pane rod;
+    @FXML
+    private Pane rope1;
+    @FXML
+    private Pane rope2;
+    @FXML
+    private Text text;
+    @FXML
+    private Pane buttons;
+    @FXML
+    private Text winStatus;
+    @FXML
+    private Text realWord;
+
+    private int mistakes;
+    private int correct;
+    private Words word = new Words();
+    private String myWord;
+    private List<String> myLetters;
+    private List<String> answer;
+
+    public HelloController() throws FileNotFoundException {
     }
+
+    public void onClick(ActionEvent event){
+
+    }
+    public void initialize() {
+        base1.setVisible(false);
+        base2.setVisible(false);
+        base3.setVisible(false);
+        pole.setVisible(false);
+        rod.setVisible(false);
+        rope1.setVisible(false);
+        rope2.setVisible(false);
+        man.setVisible(false);
+        mistakes=0;
+        correct=0;
+
+    }
+
 }
