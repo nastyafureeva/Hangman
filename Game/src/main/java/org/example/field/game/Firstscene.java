b731f1c4e178a986ba6bad5096cbdb807e522dc7
nastyafureeva <AnaDFureeva@stud.kpfu.ru>
2023-12-13 00:35:45 +0300

    the click-through is registered

---
+++ Game/src/main/java/org/example/field/game/Firstscene.java
@@ -0,0 +1,25 @@
+package org.example.field.game;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Firstscene {
+    Parent root;
+    Stage stage;
+    private ActionEvent event;
+
+
+    public void switchToCreateRoom(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("gamescene.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+
+    }
+}
--- Game/src/main/java/org/example/field/game/HelloApplication.java
+++ Game/src/main/java/org/example/field/game/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("gamescene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("firstscene.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hangman");
         stage.setScene(scene);
