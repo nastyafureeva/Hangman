d6a57c1eb0e6e7f6363917aef5697321c8b2f93b
nastyafureeva <AnaDFureeva@stud.kpfu.ru>
2023-12-12 23:54:44 +0300

    added words.txt and words class

---
+++ Game/src/main/java/org/example/field/game/Words.java
@@ -0,0 +1,29 @@
+package org.example.field.game;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Words {
+    private ArrayList<String> words;
+    private String[] letters;
+
+
+    public Words() throws FileNotFoundException {
+        words = new ArrayList<>();
+        Scanner sc = new Scanner(new File("/Users/anastasiafureeva/ОРИС/Fieldofmiracles/Game/src/main/java/org/example/field/game/Words.txt"));
+        while (sc.hasNextLine()) {
+            words.add(sc.nextLine());
+        }
+    }
+    public String getWord(){
+        return words.get(0);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Words words = new Words();
+        System.out.println(words.getWord());
+    }
+}
