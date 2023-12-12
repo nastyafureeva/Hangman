4610be9cd010edd691ce49791ff3f2f02179abe0
nastyafureeva <AnaDFureeva@stud.kpfu.ru>
2023-12-12 23:53:52 +0300

    changed stage's name

--- Game/src/main/java/org/example/field/game/HelloApplication.java
+++ Game/src/main/java/org/example/field/game/HelloApplication.java
@@ -2,6 +2,7 @@ package org.example.field.game;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,11 +12,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("gamescene.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hangman");
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
